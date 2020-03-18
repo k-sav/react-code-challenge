d8f0db21c7111452b73aa30e2acca1fc03c40f63
Kris Savoia <kris.savoia@gmail.com>
2020-03-18 11:24:27 +1100

    feat(user): init user utils

---
+++ src/util/user.js
@@ -0,0 +1,30 @@
+const localStorageKey = 'userToken'
+
+const storedUser = () => localStorage.getItem(localStorageKey) || null
+
+const setUser = user => {
+  localStorage.setItem(localStorageKey, user)
+}
+
+const removeUser = () => {
+  localStorage.removeItem(localStorageKey)
+}
+
+async function fetchUser(email = '', password = '') {
+  const response = await fetch(`${process.env.REACT_APP_API_BASE}/login`, {
+    method: 'POST',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({email, password}),
+  })
+  const status = await response.status
+  if (status === 200) {
+    return {status: 'success', user: await response.text()}
+  } else {
+    return {status: 'error', message: await response.text()}
+  }
+}
+
+export {storedUser, setUser, removeUser, fetchUser}

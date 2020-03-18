fdcbfe932b79b35f48a719f97620e1cfff1627a1
Kris Savoia <kris.savoia@gmail.com>
2020-03-18 12:44:00 +1100

    refactor(activeDevices): fetch real device data via useInterval

--- src/component/activeDevices/activeDevicesContainer.js
+++ src/component/activeDevices/activeDevicesContainer.js
@@ -1,26 +1,17 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import ActiveDevices from './activeDevicesComponent'
+import useInterval from 'util/useInterval'
+import fetchDevices from 'util/devices'
 
 const INTERVAL = 5000
-const mockDevices = [
-  {
-    id: 0,
-    name: 'Karyl',
-  },
-  {
-    id: 1,
-    name: 'Filia',
-  },
-  {
-    id: 2,
-    name: 'Coleen',
-  },
-]
 
 const Container = () => {
   const [devices, setDevices] = useState([])
 
-  useEffect(() => setDevices(mockDevices), [])
+  useInterval(async () => {
+    const {devices} = await fetchDevices()
+    setDevices(devices)
+  }, INTERVAL)
 
   return <ActiveDevices devices={devices} interval={INTERVAL}></ActiveDevices>
 }

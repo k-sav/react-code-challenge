31eb2f935a11a732d9ab51899e5b6e423116a9e4
Kris Savoia <kris.savoia@gmail.com>
2020-03-18 12:41:58 +1100

    refactor(devices): check for existence of env var

--- src/util/devices.js
+++ src/util/devices.js
@@ -1,4 +1,8 @@
 async function fetchDevices() {
+  if (!process.env.REACT_APP_API_BASE) {
+    console.error('Set REACT_APP_API_BASE in env')
+    return
+  }
   const response = await fetch(`${process.env.REACT_APP_API_BASE}/devices`, {
     method: 'GET',
     cache: 'no-cache',

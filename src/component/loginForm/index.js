d980137d69b7d000d21d455392da629129057cd8
Kris Savoia <kris.savoia@gmail.com>
2020-03-18 11:37:57 +1100

    refactor(loginForm): use the state from authContext

--- src/component/loginForm/index.js
+++ src/component/loginForm/index.js
@@ -4,22 +4,36 @@ import {jsx} from '@emotion/core'
 
 import TextField from 'ui/textField'
 import Button from 'ui/button'
-import {validEmail} from 'util/validation'
+import {AuthContext} from 'util/auth'
+import {fetchUser, setUser} from 'util/user'
+import {validEmail} from '../../util/validation'
 
 import style from './style'
 
 function LoginForm() {
+  const {state, setState} = React.useContext(AuthContext)
+  const {isPending, isError, error} = state
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   const onSubmit = async e => {
     e.preventDefault()
+    setState({...state, status: 'pending'})
+    const req = await fetchUser(email, password)
+    if (req.status === 'error') {
+      const {message} = req
+      setState({...state, status: 'error', error: message})
+    } else {
+      const {user} = req
+      setState({...state, user, status: 'success', error: null})
+      setUser(user)
+    }
   }
 
   return (
     <div css={style}>
       <form onSubmit={onSubmit}>
-        <h1>Login</h1>
+        <h1>Login </h1>
         <TextField type="email" label="Email" value={email} set={setEmail} />
         <TextField
           type="password"
@@ -27,9 +41,14 @@ function LoginForm() {
           value={password}
           set={setPassword}
         />
+        <div hidden={!error} className="feedback" aria-live="assertive">
+          {error}
+        </div>
         <Button
           version="primary"
-          disabled={!email || !password || !validEmail(email)}
+          disabled={
+            isPending || isError || !email || !password || !validEmail(email)
+          }
         >
           Login
         </Button>
